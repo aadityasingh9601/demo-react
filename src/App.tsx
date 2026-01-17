08a647b6243ea2307507a5c6d3afe9d8d02f2528
Aaditya Singh <aadityasingh9601@gmail.com>
2026-01-17 11:19:25 +0000

    test ci-cd pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div>This is our react app.</div>;
+  return <div>This is our react app deployed on AWS S3 & Cloudfront.</div>;
 }
 
 export default App;
